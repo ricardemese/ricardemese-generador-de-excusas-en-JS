178e93a5fae0a5707537231f3cb846349b1b5e31
Ricard Mesegué <ricardm1995@gmail.com>
2024-08-05 18:35:13 +0000

    modified correctly

--- src/app.js
+++ src/app.js
@@ -5,37 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "The mailman", "My bird", "My neighboord"];
-let action = ["ate", "peed", "crushed", "broke", "smoke"];
-let what = ["my homework", "my phone", "the car", "my computer", "my lunch"];
-let when = [
-  "before the class",
-  "when I was sleeping",
-  "while I was exercising",
-  "during my lunch",
-  "while I was praying"
-];
+function generateExcuse() {
+  let who = [
+    "The dog",
+    "My grandma",
+    "The mailman",
+    "My bird",
+    "My neighboord"
+  ];
+  let action = ["ate", "peed", "crushed", "broke", "smoke"];
+  let what = ["my homework", "my phone", "the car", "my computer", "my lunch"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
 
-function getRandomNumber(a, b) {
-  return Math.floor(Math.random() * (b - a + 1)) + a;
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  document.getElementById("excuse").innerHTML = excuse;
 }
 
-function getRandom(anyArray) {
-  let max = anyArray.length - 1;
-  let min = 0;
-  let random = getRandomNumber(min, max);
-  return anyArray[random];
-}
-
-window.onload = function() {
-  //write your code here
-  console.log("Who reads it, is silly!");
-  document.querySelector("#excuse").innerHTML =
-    getRandom(who) +
-    " " +
-    getRandom(action) +
-    " " +
-    getRandom(what) +
-    " " +
-    getRandom(when);
-};
+window.onload = generateExcuse();
